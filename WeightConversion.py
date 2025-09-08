b4491cb83c8fd839625e319342f1330ec662f79a
J1A <3151637181@qq.com>
2025-09-08 16:43:03 +0800

    Update and rename main.py to WeightConversion.py

--- WeightConversion.py
+++ WeightConversion.py
@@ -1,11 +1,11 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
 #千克与英镑重量转换
 ZHstr=input("请输入带有单位要转换的质量：")
-if ZHstr[-2] in ['k'] or ZHstr[-2] in ['千']:
+if ZHstr[-2] in ['k'] or ZHstr[-2] in ['千'] or ZHstr[-2] in ['公']:
     R = (eval (ZHstr[0:-2]) *2.2046 )
-    print("转换后的质量为{:.3f}英镑".format(R))
+    print("对应的英制重量为{:.3f}英镑".format(R))
 elif ZHstr[-2] in ['p'] or ZHstr[-2] in ['英']:
     M = (eval (ZHstr[0:-2] /2.2046 ))
-    print("转换后的质量为{:.3f}千克".format(M))
+    print("对应的公制重量为{:.3f}公斤".format(M))
 else:
     print("输入的格式错误")
